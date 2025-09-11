d4c76e3b8acb6d446ce0d221cce4a82b4064a6ac
CodeRonin <vincentcss16@gmail.com>
2025-09-11 21:07:47 +0200

    refactor: update styles and dependencies for improved layout and responsiveness

--- src/features/header/components/logo/logo.jsx
+++ src/features/header/components/logo/logo.jsx
@@ -6,7 +6,7 @@ export default function LogoComponents() {
 
     return (
         <div className={styles.logo}>
-            <img src={logo} alt="logo" className={styles.logo}/>
+            <img src={logo} alt="logo" className={styles.logo_img}/>
         </div>
     )
 }
