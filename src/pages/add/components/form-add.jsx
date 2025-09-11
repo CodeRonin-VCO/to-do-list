94d36b69f3ba71bd8b1d651257d5d9ec695e1a9c
CodeRonin <vincentcss16@gmail.com>
2025-09-11 15:46:08 +0200

    form add (fctn)

--- src/pages/add/components/form-add.jsx
+++ src/pages/add/components/form-add.jsx
@@ -1,27 +1,69 @@
+import { useActionState } from "react";
 import styles from "./form-add.module.css";
+import { nanoid } from "nanoid";
 
 export default function FormAddTasks() {
-    
+    function onSubmitForm(prevState, formData) {
+        const requiredFields = ["title", "theme", "desc", "date"];
+        const data = { userID: nanoid(), file: formData.get("file") }
+
+        const errors = {};
+        for (const field of requiredFields) {
+            const value = formData.get(field);
+            data[field] = value;
+            if (!value) {
+                errors[field] = "This field must be completed.";
+            }
+        }
+        if (Object.keys(errors).length > 0) {
+            return { data: null, errors, message: "Invalid data. All fields are required." }
+        }
+
+        return { data: data, message: "Your message has been sent successfully.", errors }
+    }
+
+    const initialData = { data: null, message: null, errors: {} }
+    const [state, handleForm, isPending] = useActionState(onSubmitForm, initialData)
 
     return (
         <div className={styles.container_form}>
-            <form action="" className={styles.form}>
+            <form action={handleForm} className={styles.form}>
                 <div className={styles.container_input}>
                     <label htmlFor="title">Title:</label>
                     <input type="text" id="title" name="title" />
+
+                    {/* Error message */}
+                    {state.errors.title && (<span className={styles.error_msg}>{state.errors.title}</span>)}
+                    {/* ------------- */}
                 </div>
+
                 <div className={styles.container_input}>
                     <label htmlFor="theme">Theme:</label>
                     <input type="text" id="theme" name="theme" />
+
+                    {/* Error message */}
+                    {state.errors.theme && (<span className={styles.error_msg}>{state.errors.theme}</span>)}
+                    {/* ------------- */}
                 </div>
+
                 <div className={styles.container_input}>
                     <label htmlFor="desc">Description:</label>
                     <textarea name="desc" id="desc"></textarea>
+
+                    {/* Error message */}
+                    {state.errors.desc && (<span className={styles.error_msg}>{state.errors.desc}</span>)}
+                    {/* ------------- */}
                 </div>
+
                 <div className={styles.container_input}>
                     <label htmlFor="date">Date:</label>
                     <input type="datetime-local" id="date" name="date" />
+
+                    {/* Error message */}
+                    {state.errors.date && (<span className={styles.error_msg}>{state.errors.date}</span>)}
+                    {/* ------------- */}
                 </div>
+
                 <div className={styles.container_input}>
                     <label htmlFor="prior">Priority:</label>
                     <select name="prior" id="prior">
@@ -29,12 +71,19 @@ export default function FormAddTasks() {
                         <option value="low">Low</option>
                         <option value="high">High</option>
                     </select>
+
                 </div>
+
                 <div className={styles.container_input}>
                     <label htmlFor="file" className={styles.custom_file_upload}>Upload file : Browse 📎</label>
                     <input type="file" id="file" name="file" />
                 </div>
+
                 <button className={styles.btn} type="submit">Add new task</button>
+
+                {/* Error message */}
+                {state.message && (<p className={styles.message}>{state.message}</p>)}
+                {/* ------------- */}
             </form>
         </div>
     )
--- src/pages/contact/components/form-contact.jsx
+++ src/pages/contact/components/form-contact.jsx
@@ -8,19 +8,18 @@ export default function FormContact() {
 
     // Gestion formulaire
     function onSubmitForm(prevState, formData) {
-        const data = {
-            userID: nanoid(),
-            name: formData.get("name"),
-            email: formData.get("email"),
-            subject: formData.get("subject"),
-            msg: formData.get("msg")
-        }
+        const requiredFields = ["name", "email", "subject", "msg"]
+
+        const data = { userID: nanoid() }
 
         const errors = {};
-        if (!data.name) errors.name = "The first name is required.";
-        if (!data.email) errors.email = "The email is required.";
-        if (!data.subject) errors.subject = "The email is required.";
-        if (!data.msg) errors.msg = "The message is required.";
+        for (const field of requiredFields) {
+            const value = formData.get(field);
+            data[field] = value;
+            if (!value) {
+                errors[field] = "This field must be completed.";
+            }
+        }
         if (Object.keys(errors).length > 0) {
             return { data: null, errors, message: "Invalid data. All fields are required." }
         }
@@ -29,34 +28,53 @@ export default function FormContact() {
     }
 
     const initialData = { data: null, message: null, errors: {} }
-    const [state, handleform, isPending] = useActionState(onSubmitForm, initialData)
+    const [state, handleForm, isPending] = useActionState(onSubmitForm, initialData)
 
     return (
         <>
             <div className={styles.container_form}>
-                <form action={handleform} className={styles.form}>
+                <form action={handleForm} className={styles.form}>
                     <div className={styles.container_input}>
                         <label htmlFor="name">Your name</label>
                         <input type="text" id="name" name="name" />
+
+                        {/* Error message */}
                         {state.errors.name && (<span className={styles.error_msg}>{state.errors.name}</span>)}
+                        {/* ------------- */}
                     </div>
+
                     <div className={styles.container_input}>
                         <label htmlFor="email">Email address</label>
                         <input type="email" id="email" name="email" />
+
+                        {/* Error message */}
                         {state.errors.email && (<span className={styles.error_msg}>{state.errors.email}</span>)}
+                        {/* ------------- */}
                     </div>
+
                     <div className={styles.container_input}>
                         <label htmlFor="subject">Subject</label>
                         <input type="text" id="subject" name="subject" />
+
+                        {/* Error message */}
                         {state.errors.subject && (<span className={styles.error_msg}>{state.errors.subject}</span>)}
+                        {/* ------------- */}
                     </div>
+
                     <div className={styles.container_input}>
                         <label htmlFor="msg">Message</label>
                         <textarea name="msg" id="msg"></textarea>
+
+                        {/* Error message */}
                         {state.errors.msg && (<span className={styles.error_msg}>{state.errors.msg}</span>)}
+                        {/* ------------- */}
                     </div>
+
                     <button className={styles.btn} type="submit">Send Message</button>
+
+                    {/* Error message */}
                     {state.message && (<p className={styles.message}>{state.message}</p>)}
+                    {/* ------------- */}
                 </form>
             </div>
         </>
